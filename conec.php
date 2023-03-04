71ebc4dba02f64cce38140154a39c7a265bfc40c
joaocss <joaocss@dcomp.ufs.br>
2023-03-04 17:29:30 +0044

    atualização conec

--- conec.php
+++ conec.php
@@ -2,7 +2,7 @@
 define('HOST','teste.cc0lxxnmclur.us-east-2.rds.amazonaws.com');
 define('USER','root');
 define('PASS','teste123');
-define('BASE','cadastro');
+define('BASE','db_teste');
 
 $conn = new MySQLi(HOST,USER,PASS,BASE);
 
